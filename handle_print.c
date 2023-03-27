6f2add6a4f579e9d65ea0725d1db6fa9c843490b
HalaHassan2309 <123985522+HalaHassan2309@users.noreply.github.com>
2023-03-27 20:25:25 +0200

    task

--- handle_print.c
+++ handle_print.c
@@ -46,5 +46,4 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		unknow_len += write(1, &fmt[*ind], 1);
 		return (unknow_len);
 	}
-	return (printed_chars);
-}
+	return (printed_chars); }
